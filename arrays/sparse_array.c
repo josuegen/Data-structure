b6e015cbe8c167b29fb39d1b00cf84112534407b
Josué Velázquez Gen <jvelazquezg1501@alumno.ipn.mx>
2018-04-03 11:43:07 -0500

    Update sparse_array.c

--- arrays/sparse_array.c
+++ arrays/sparse_array.c
@@ -2,9 +2,9 @@
 palabras en la primera serie
 */
 /*
-There is a collection of  strings ( There can be multiple occurences of a particular string ). 
-Each string's length is no more than  characters. There are also  queries. 
-For each query, you are given a string, and you need to find out how many times this string occurs in the given collection of  strings.
+There is a collection of N strings ( There can be multiple occurences of a particular string ). 
+Each string's length is no more than 20 characters. There are also Q queries. 
+For each query, you are given a string, and you need to find out how many times this string occurs in the given collection of N strings.
 /*
 
 #include <stdio.h>
